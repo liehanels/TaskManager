84ce5acb6143cbe5a33564448f8d8eaccc4baff6
Liehan Els <liehan13@gmail.com>
2025-04-17 16:55:33 +0200

    Test

--- app/src/main/java/com/liehan/taskmanager/MainActivity.kt
+++ app/src/main/java/com/liehan/taskmanager/MainActivity.kt
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         //code starts here
         val Username = "Lee"
         val Password = "Qwerty1"
